2e9424bee8703051e73b6ff7c54c54e0ea0e966a
Anshita1702 <jainanshita705@gmail.com>
2024-09-06 09:40:56 +0530

    first commit

---
+++ src/components/Home.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Sidenav from "./templates/Sidenav";
+
+const Home = () => {
+  document.title = "Movie app | Homepage";
+  return <> 
+     <Sidenav/>
+      <div className="w-[80%] h-full "></div>
+  
+  </>;
+};
+
+export default Home;
---
+++ src/components/templates/Sidenav.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Sidenav = () => {
+  return <div className="w-[20%] h-full border-r-2 border-zinc-400 p-3">
+
+<i class=" text-[#6556CD] ri-tv-fill text-2xl"></i>
+<span className="text-2xl">SCSDB</span>
+  </div>;
+};
+
+export default Sidenav;
